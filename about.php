b8e0cb63b120961b8bfa9985e909f623aadee20b
mohiteakanksha <143405012+mohiteakanksha@users.noreply.github.com>
2023-08-28 17:43:11 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,156 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="box">
+         <img src="images/img.jpg" alt="">
+         <h3>why choose us?</h3>
+         <p>Where affordability meets fast delivery, Foodie Zone Services is your one stop service for quality Foodie Zone delivery right across India. As a leading industry name in Foodie Zone supply, Foodie Zone Services prides itself on offering an unrivalled service.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+      <div class="box">
+         <img src="images/img2.jpg" alt="">
+         <h3>what we provide?</h3>
+         <p>Aside form their natural good taste and great curnchy texture alongside wonderful colors and fragrances eating a large serving of fresh.</p>
+         <a href="shop.php" class="btn">our shop</a>
+      </div>
+
+   </div>
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">clients reivews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <p> The delivery boy was here on time, delivered the parcel on time. He was courteous and understanding, and we were very happy! </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <p> I got everything I ordered. I got it in a very timely manner. I would definitely recommended to my friends and family.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <p>A totally brilliant service from start to finish. I wish every experience was as good as you guys. Can’t recommend you highly enough.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <p> Our package was an urgent same day delivery request. The service was excellent, with the package being collected earlier than hope.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <p>My parcels were picked up and delivered on time and handled with care. Thank you, I would highly recommend and will definitely use again.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>Pick up was on time and the parcel was delivered before our deadline. Very happy with the service we have used you in the past and will use you again.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,205 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_to_wishlist'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+
+   $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+   $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_wishlist_numbers->rowCount() > 0){
+      $message[] = 'already added to wishlist!';
+   }elseif($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+      $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+      $message[] = 'added to wishlist!';
+   }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $pid = $_POST['pid'];
+   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+   $p_name = $_POST['p_name'];
+   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+   $p_price = $_POST['p_price'];
+   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+   $p_image = $_POST['p_image'];
+   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+   $p_qty = $_POST['p_qty'];
+   $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+   $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+   $check_cart_numbers->execute([$p_name, $user_id]);
+
+   if($check_cart_numbers->rowCount() > 0){
+      $message[] = 'already added to cart!';
+   }else{
+
+      $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+      $check_wishlist_numbers->execute([$p_name, $user_id]);
+
+      if($check_wishlist_numbers->rowCount() > 0){
+         $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+         $delete_wishlist->execute([$p_name, $user_id]);
+      }
+
+      $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+      $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+      $message[] = 'added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="home-bg">
+
+   <section class="home">
+
+      <div class="content">
+         <span>Order tasty & fresh food</span>
+         <h3>Cooking Up Your New Cravings</h3>
+         <p>Call to place your order for great home-style cooking!</p>
+         <a href="about.php" class="btn">about us</a>
+      </div>
+
+   </section>
+
+</div>
+
+<section class="home-category">
+
+   <h1 class="title">category</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/snacks.jpg" alt="">
+         <a href="category.php?category=snacks" class="btn">Snacks</a>
+      </div>
+      <div class="box">
+         <img src="images/chinese.jpg" alt="">
+         <p><br></p>        
+         <a href="category.php?category=chinese" class="btn">Chinese</a>
+      </div>
+      <div class="box">
+         <img src="images/veg-meal.jpg" alt="">      
+         <a href="category.php?category=veg meal" class="btn">Veg meal</a>
+      </div>
+      <div class="box">
+         <img src="images/nonveg-meal.jpg" alt="">
+         <p><br></p>
+         <a href="category.php?category=nonveg meal" class="btn">Nonveg meal</a>
+      </div>
+      <div class="box">
+         <img src="images/beverages.jpg" alt="">
+         <a href="category.php?category=beverages" class="btn">Beverages</a>
+      </div>
+      <div class="box">
+         <img src="images/sweets.jpg" alt="">
+         <a href="category.php?category=sweets" class="btn">Sweets</a>
+      </div>
+      <div class="box">
+         <img src="images/bakery-product.jpg" alt="">
+         <a href="category.php?category=bakery" class="btn">Bakery Products</a>
+      </div>
+      <div class="box">
+         <img src="images/dairy-product.jpg" alt="">
+         <a href="category.php?category=dairy" class="btn">Dairy Products</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+      $select_products->execute();
+      if($select_products->rowCount() > 0){
+         while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){ 
+   ?>
+   <form action="" class="box" method="POST">
+      <div class="price">$<span><?= $fetch_products['price']; ?></span>/-</div>
+      <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+      <div class="name"><?= $fetch_products['name']; ?></div>
+      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+      <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+      <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+      <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+      <input type="number" min="1" value="1" name="p_qty" class="qty">
+      <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
+      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+   </form>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no products added yet!</p>';
+   }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
